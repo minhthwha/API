42a9c2cbe87974b4f7d4985b1267b1720f18c2b9
thw <minhthwha@gmail.com>
2023-02-17 10:21:06 +0700

    delete jwtcontroller, edit usercontroller

--- ToDoList/Controllers/UsersController.cs
+++ ToDoList/Controllers/UsersController.cs
@@ -1,21 +1,28 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BCrypt.Net;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using ToDoList.DTOs;
 using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
-    [Authorize] // Thêm thuộc tính ủy quyền => Tất cả các API trong controller đều được bảo mật bằng token.
     [Route("api/Users")]
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
         private readonly ToDoListDbContext _dbContext;
 
         // Khai báo biến để dùng database.
-        public UsersController(ToDoListDbContext dbContext)
+        public UsersController(IConfiguration configuration, ToDoListDbContext dbContext)
         {
+            _configuration = configuration;
             _dbContext = dbContext;
         }
 
@@ -31,8 +38,8 @@ namespace ToDoList.Controllers
                 PhoneNumber = userRegistration.PhoneNumber,
                 Address = userRegistration.Address,
                 UserName = userRegistration.UserName,
-                Password = userRegistration.Password,
-            };
+                Password = BCrypt.Net.BCrypt.HashPassword(userRegistration.Password)
+            };            
 
             // Gán query bằng các giá trị username đã có trong database.
             var query = _dbContext.Users.FirstOrDefault(t => t.UserName == userRegistration.UserName);
@@ -54,15 +61,42 @@ namespace ToDoList.Controllers
         [HttpPost("LogIn")]
         public IActionResult LogIn(LogInRequest userLogIn)
         {
-            var query = _dbContext.Users.FirstOrDefault(t => t.UserName == userLogIn.UserName && t.Password == userLogIn.Password);
-            if(query == null)
+            // Kiểm tra tên đăng nhập có tồn tại không.
+            var user = _dbContext.Users.FirstOrDefault(t => t.UserName == userLogIn.UserName);
+            if(user != null)
             {
-                return BadRequest("Tên người dùng hoặc mật khẩu không trùng khớp!");
+                // Nếu tên đăng nhập có trong database thì verify mật khẩu được mã hóa trong database. 
+                bool verify = BCrypt.Net.BCrypt.Verify(userLogIn.Password, user.Password);
+                if (verify == true)
+                {
+                    // Tạo chi tiết claims dựa trên thông tin của người dùng.
+                    var claims = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim("UserId", user.UserId.ToString()),
+                        new Claim("Name", user.Name),
+                        new Claim("Email", user.Email),
+                        new Claim("PhoneNumber", user.PhoneNumber),
+                        new Claim("Address", user.Address),
+                        new Claim("UserName", user.UserName),
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var logIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var jwt = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], claims, signingCredentials: logIn);
+
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+                }
+                else
+                {
+                    return BadRequest("Thông tin không hợp lệ!");
+                }
             }
             else
             {
-                return Ok("Đăng nhập thành công!");
-            }
+                return BadRequest("Tên người dùng hoặc mật khẩu không trùng khớp!");
+            }          
         }
     }
 }
